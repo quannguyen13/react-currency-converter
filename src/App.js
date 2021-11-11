ec9a190b77bb8a5db40f12dd5250ec7699443d5a
quannguyen <waynenguyenus@gmail.com>
2021-11-10 21:22:01 -0800

    test upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React,{useEffect} from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+
+const BASE_URL = process.env.REACT_APP_SERVER_URL;
 
 function App() {
+// console.log(BASE_URL);
+
+
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  },[])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+   <h1>Converter</h1>
+    <CurrencyRow />
+    <div className="equal">=</div>
+    <CurrencyRow />
     </div>
   );
 }
---
+++ src/CurrencyRow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CurrencyRow = () => {
+    return (
+        <div>
+           <input type="number" className='input'/>
+            <select>
+                <option value="Hi">Hi</option>
+            </select>
+        </div>
+    )
+}
+
+export default CurrencyRow
